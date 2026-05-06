f208c9b921076fb3ba810779f11ea26dd5173d63
theodrom <theo.matskas@limetta.se>
2026-05-06 16:16:56 +0200

    fix(admin): make TeamStatsDto properties nullable for correct Blazor validation and input preservation

--- CoachHoopsAI.Admin/Models/TeamStatsDto.cs
+++ CoachHoopsAI.Admin/Models/TeamStatsDto.cs
@@ -5,20 +5,20 @@ namespace CoachHoopsAI.Admin.Models
     public class TeamStatsDto
     {
         [Required]
-        public int Points { get; set; }
+        public int? Points { get; set; }
         [Range(0, 1)]
-        public double FieldGoalPercentage { get; set; }
+        public double? FieldGoalPercentage { get; set; }
         [Range(0, 1)]
-        public double ThreePointPercentage { get; set; }
+        public double? ThreePointPercentage { get; set; }
         [Required]
-        public int ThreePointAttempts { get; set; }
+        public int? ThreePointAttempts { get; set; }
         [Required]
-        public int OffensiveRebounds { get; set; }
+        public int? OffensiveRebounds { get; set; }
         [Required]
-        public int DefensiveRebounds { get; set; }
+        public int? DefensiveRebounds { get; set; }
         [Required]
-        public int Turnovers { get; set; }
+        public int? Turnovers { get; set; }
         [Required]
-        public int Fouls { get; set; }
+        public int? Fouls { get; set; }
     }
 }
